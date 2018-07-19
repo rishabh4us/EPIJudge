9317d711369faead5ff691444db823d0173f5665
Rishabh Sonthalia <rishabh.sonthalia@ayasdi.com>
2018-07-18 23:30:04 -0700

    square root, power with bits, reverse sublist

--- epi_judge_java/epi/PowerXY.java
+++ epi_judge_java/epi/PowerXY.java
@@ -1,11 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class PowerXY {
   @EpiTest(testDataFile = "power_x_y.tsv")
   public static double power(double x, int y) {
     // TODO - you fill in here.
-    return 0.0;
+    // f = x ^ y ;
+    // log f = y* log x
+//    int sign = y % 2 == 0 ? 1 : (int) Math.signum(x);
+//    return sign * Math.exp(y * Math.log(Math.abs(x)));
+
+//    OR
+    if (y == 0) return 1;
+    if (y<0){
+      x= 1/x;
+      y  = -1*y;
+    }
+    double result = 1;
+    while (y > 0) {
+      if ((y & 1)==1) {
+        result *= x;
+      }
+      x *= x;
+      y >>>= 1;
+    }
+    return result;
+//    return 0.0;
   }
 
   public static void main(String[] args) {
